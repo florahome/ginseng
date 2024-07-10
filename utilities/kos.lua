a1dc30929d762a1ab05b396f21983c1040e7e001
florahome <florahomeee@gmail.com>
2024-07-10 01:59:43 -0400

    Update kos.lua

--- utilities/kos.lua
+++ utilities/kos.lua
@@ -2,5 +2,12 @@
 --you are free to review all KOS. they are open sourced and displayed for a reason.
 
 return {
-  [1] = "reason"
+  --group 1: harassment
+  --what they did: this group of individuals harassed and intimidated several individuals. The group then went on to claim these individual(s) that they were harassing were 'overreacting' and then threw several more insults.
+  [3434024918] = "nil", -- teendolII
+  [1932688906] = "nil", -- blinker4kevi
+  [5593889110] = "nil", -- mugiwarakevi
+  [382146618] = "nil", -- panicattheoofs
+  [226225476] = "nil", -- stupidviIIe
+  [4323265152] = "nil", -- svegliatio
 }
